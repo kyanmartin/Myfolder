64534124d0ac0ea1c70c140951662c74439c4421
Kyan Martin <kyanjmartin@gmain.com>
2026-02-22 18:58:54 -0800

    V3 of calculator

--- calculator.py
+++ calculator.py
@@ -4,11 +4,15 @@ while thing_to_do not in ["*", "/", "+", "-"]:
     thing_to_do = input("Invalid operation. Please choose one of the following: (*, /, +, -):")
 first_number = int(input("What is the first number? "))
 second_number = int(input("What is the second number? "))
-answer = first_number * second_number if thing_to_do == "*" else \
+if thing_to_do == "/" and second_number == 0:
+    print("Error: Division by zero is not allowed.")
+else:
+    answer = first_number * second_number if thing_to_do == "*" else \
         first_number / second_number if thing_to_do == "/" else \
         first_number + second_number if thing_to_do == "+" else \
         first_number - second_number if thing_to_do == "-" else "Invalid operation"
-print("The answer is: " + str(answer))
+
+    print("The answer is: " + str(answer))
 go_again = input("Do you want to go again? (y/n) ")
 while go_again not in ["y", "n"]:
     go_again = input("Invalid input. Please enter 'y' for yes or 'n' for no: ")
